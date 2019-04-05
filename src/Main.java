8418dde90b2629ee2e97897273141c0e747a14b8
Justine S <justines@Justines-MacBook.local>
2019-04-05 19:50:35 +0300

    Task 2 (extra)

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        Task 2: Admision
-//        The potential clients are now piling in, and it is becoming painfully obvious, that the rented room will soon become too small,
-//        as it can only house 100 people. It would be impolite from us to turn people away,
-//        so let's make a program that does the dirty work for us.
-//
-//        Create an appliction that:
-//
-//        reads as input: the amount of people in the room
-//        reads as input: the amount of people still in the queue
-//        prints if every person in the queue can fit in the room
+//        Task 2 (extra):
+//        Instead of just printing if everyone can make it, print for every person in the queue, if they can make it in
 
         int house = 100;
 
@@ -23,9 +15,13 @@ public class Main {
         System.out.print("How many people are in the queue: ");
         int queue = sc.nextInt();
 
-        if (room + queue > 100) {
-            System.out.println("My apologies, but not all of the people standing in queue can fit in the room :(");
-        } else System.out.println("Perfect! All the people in the queue can fit in the room :)) ");
+        for (int i = 1; i <= queue; i++) {
+
+            if (room + i > 100) {
+                System.out.println("can't fit");
+            } else System.out.println("fit");
+
+        }
 
 
     }
