e1b095ab3e7c30ed60078de3ec7aaffc7564aeb3
Justine S <justines@Justines-MacBook.local>
2019-04-05 20:59:13 +0300

    Tast 5 (extra)

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        Task 5: Mobile market
-//        With the brand all set up, you decided to make a run in the smartphone app market.
-//        You outsourced the development of your mobile app, but of course the company tasked with it was incompetent.
-//        Credit card encryption is seriously flawed and it is up to you to fix it.
-//        You decide to pick the current top-notch encryption algorithm which goes like this:
+//        ask 5 (extra):
+//        To increase security ignore any letters and dashes, and also consider the card number to "loop" around itself
+//        (a.k.a.imagine that the first digit follows the last one).
 //
-//        Looking at the credit card number digit-by-digit create a single "hash" by summing together all digits that are
-//        followed by the same digit.
-//
-//        Create an application that:
-//
-//        lets the user input their credit card number
-//        outputs the hash
+
 
         System.out.print("Please enter your credit card number: ");
         String number = sc.next();
 
+        String security = number.replaceAll("[^0-9\\+]", "");
+
+        security += security.charAt(0);
+
         int hash = 0;
 
-        for (int i = 0; i < (number.length()-1); i++) {
-            if (number.charAt(i) == number.charAt(i + 1)) {
-                hash += (number.charAt(i) - 48);
+        for (int i = 0; i < (security.length() - 1); i++) {
+            if (security.charAt(i) == security.charAt(i + 1)) {
+                hash += (security.charAt(i) - 48);
             }
         }
         System.out.println(hash);
