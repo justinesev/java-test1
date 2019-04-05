703fc453dbda755f4d4bb0f0104fd5b95fc19e4b
Justine S <justines@Justines-MacBook.local>
2019-04-05 19:45:25 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-//        Task 1: Grand opening!
-//        It is the first day of your new business. Time to do a grand opening! Sadly the robot that you hired to do meet-and-greet is broken.
-//        The main module is missing and it is up to you to write the core functionality and save the opening ceremony.
-//
-//        Create an application that:
-//
-//        greets the user and asks for their name
-//        politely echoes the user's name back, and invites them into the opening ceremony
-        
         Scanner sc = new Scanner(System.in);
-        System.out.print("Hello! What is your name: ");
-        String name = sc.nextLine();
-        System.out.println("Oh, nice to meet you " + name + "! Please come in to the opening ceremony.");
+
+//        Task 2: Admision
+//        The potential clients are now piling in, and it is becoming painfully obvious, that the rented room will soon become too small,
+//        as it can only house 100 people. It would be impolite from us to turn people away,
+//        so let's make a program that does the dirty work for us.
+//
+//        Create an appliction that:
+//
+//        reads as input: the amount of people in the room
+//        reads as input: the amount of people still in the queue
+//        prints if every person in the queue can fit in the room
+
+        int house = 100;
+
+        System.out.print("How many people are in the room: ");
+        int room = sc.nextInt();
+        System.out.print("How many people are in the queue: ");
+        int queue = sc.nextInt();
+
+        if (room + queue > 100) {
+            System.out.println("My apologies, but not all of the people standing in queue can fit in the room :(");
+        } else System.out.println("Perfect! All the people in the queue can fit in the room :)) ");
+
 
     }
 }
