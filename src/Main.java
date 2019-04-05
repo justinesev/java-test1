878431998a3d73fe99a9454b54a4b1357ebca282
Justine S <justines@Justines-MacBook.local>
2019-04-05 19:36:43 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        boolean x = false || true && 5 >= 5;
-        System.out.println(x);
+//        Task 1: Grand opening!
+//        It is the first day of your new business. Time to do a grand opening! Sadly the robot that you hired to do meet-and-greet is broken.
+//        The main module is missing and it is up to you to write the core functionality and save the opening ceremony.
+//
+//        Create an application that:
+//
+//        greets the user and asks for their name
+//        politely echoes the user's name back, and invites them into the opening ceremony
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hello! What is your name: ");
+        String name = sc.nextLine();
+        System.out.println("Oh, nice to meet you " + name + "! Please come in to the opening ceremony.");
+
     }
 }
